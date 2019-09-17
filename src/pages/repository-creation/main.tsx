664dbb517727512670aeab42bf62fe74d25a77ee
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-09-17 16:13:35 -0700

    Use fontawesome node module

--- src/pages/repository-creation/main.tsx
+++ src/pages/repository-creation/main.tsx
@@ -1,5 +1,5 @@
-import "../../vendor/fontawesome/css/fontawesome.min.css";
-import "../../vendor/fontawesome/css/solid.min.css";
+import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
+import "@fortawesome/fontawesome-free/css/solid.min.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../../styles/main.scss";
