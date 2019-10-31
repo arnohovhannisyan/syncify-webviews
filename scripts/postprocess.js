e13eab4ccf441c0af0c4b06f2f39089583e70290
Arno Hovhannisyan <arnohovhannisyan0@gmail.com>
2019-10-30 17:11:57 -0700

    Improve postprocess script

--- scripts/postprocess.js
+++ scripts/postprocess.js
@@ -4,13 +4,14 @@ const { readFile, writeFile } = require("fs-extra");
 async function start() {
   const file = `${resolve(__dirname, "../dist")}/index.html`;
   const contents = await readFile(file, "utf-8");
-  const newContents = contents
-    .replace(/<script src="(.*?)"/, '<script src="@PWD/$1"')
-    .replace(
-      /<link rel="stylesheet" href="(.*?)"/,
-      '<link rel="stylesheet" href="@PWD/$1"'
-    );
-  await writeFile(file, newContents);
+
+  await writeFile(
+    file,
+    contents.replace(
+      /<(link|script)(\srel="stylesheet")? ?(src|href)="(.*?)"/g,
+      '<$1$2 $3="@PWD/$4"'
+    )
+  );
 }
 
 start();
